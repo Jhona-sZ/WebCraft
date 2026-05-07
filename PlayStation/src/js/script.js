ffc22da87f6895d04b82254940fc8cbea63a0274
Jhonatan Sánchez Zea <jhonatansanchezzea@gmail.com>
2026-05-07 23:30:25 +0200

    Add files via upload

---
+++ PlayStation/src/js/script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById('menu-btn')
+const mobileMenu = document.getElementById('mobile-menu')
+const menuIcon = document.getElementById('menu-icon')
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+    menuIcon.classList.toggle('fa-bars')
+    menuIcon.classList.toggle('fa-xmark')
+})
